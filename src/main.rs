4f0e1d4d3050b2915235cdb565fd1fa9f9b76eb8
reecewayt <reecewayt96@gmail.com>
2025-02-10 19:18:41 -0800

    led feature is working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-#![deny(unsafe_code)]
+//@file: src/main.rs
+
 #![no_main]
 #![no_std]
 
 mod orientation;
 mod power;
+mod screen; 
 
 use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};
@@ -24,13 +26,16 @@ use lsm303agr::{
 
 use crate::orientation::OrientationManager;
 use crate::power::PowerManager;
+use crate::screen::ScreenManager;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
-    let mut timer = Timer::new(board.TIMER0); //TODO: Implement timer interrupt
     let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    
+    // Initialize screen manager with TIMER1
+    let screen_manager = ScreenManager::new(board.TIMER1, board.display_pins);
 
     // Code from documentation
     let mut sensor = Lsm303agr::new_with_i2c(i2c);
@@ -38,6 +43,9 @@ fn main() -> ! {
     sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
     
     let mut orientation_manager = OrientationManager::new();
+
+    // Use TIMER0 for timing
+    let mut timer = Timer::new(board.TIMER0);
     
     loop {
         let current_time = timer.read();
@@ -53,6 +61,10 @@ fn main() -> ! {
                 current_time as i32
             ) {
                 rprintln!("New orientation: {:?}", new_orientation);
+
+                // Update screen orientation
+                //screen_manager.clear();
+                screen_manager.update(new_orientation);
             }
         }
     }
